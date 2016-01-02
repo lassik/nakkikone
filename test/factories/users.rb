6f254e281bc6029178f1dbcaf1995fe043ef1a56
Sami Airaksinen <samiaira@gmail.com>
2016-01-02 17:14:52 +0200

    fix nakki type info functional tests

--- test/factories/users.rb
+++ test/factories/users.rb
@@ -1,12 +1,43 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
-# FactoryGirl.define do
-#   factory :user do
-#     password 'password'
-#     password_confirmation 'password'
+FactoryGirl.define do
+  factory :user do
+    password 'password'
+    password_confirmation 'password'
+    role "user"
 
-#     sequence :email do |n| 
-#       "user#{n}@example.com"
-#     end
-#   end
-# end
+    sequence :name do |n| 
+      "user#{n}"
+    end
+    
+    sequence :nick do |n| 
+      "user#{n}"
+    end
+    
+    sequence :email do |n| 
+      "user#{n}@example.com"
+    end
+
+    number "040123456"
+  end
+
+  factory :admin, class: User do
+    password 'password'
+    password_confirmation 'password'
+    role "admin"
+
+    sequence :name do |n| 
+      "user#{n}"
+    end
+    
+    sequence :nick do |n| 
+      "user#{n}"
+    end
+    
+    sequence :email do |n| 
+      "user#{n}@example.com"
+    end
+
+    number "040123456"
+  end
+end
--- test/functional/nakkitype_info_controller_test.rb
+++ test/functional/nakkitype_info_controller_test.rb
@@ -5,8 +5,15 @@ describe NakkitypeInfosController do
     ActiveSupport::JSON.decode @response.body
   end
 
-  setup do
+  def login_as(user)
+    @request.session[:user_id] = user ? user.id : nil
+  end
+
+  before do
     FactoryGirl.create_list(:nakkitype_info, 2)
+    test_user = FactoryGirl.create(:admin)
+    login_as(test_user)
+
     @request.headers['Accept'] = Mime::JSON
     @request.headers['Content-Type'] = Mime::JSON.to_s
   end
@@ -36,33 +43,37 @@ describe NakkitypeInfosController do
   it "updates properly one info" do
     update_id = NakkitypeInfo.all.last[:id]
 
-    put :update, { :id => update_id, :title => "another title"}, :format => :json
+    put :update, :id => update_id, :title => "another title", :description => "another description", :format => :json
     assert_response :success
 
     assert_equal "another title", NakkitypeInfo.all.last[:title]
     assert_equal "another title", json_response["title"]
+    assert_equal "another description", NakkitypeInfo.all.last[:description]
+    assert_equal "another description", json_response["description"]
   end
 
   it "fails to update with invalid values" do
     update_id = NakkitypeInfo.all.last[:id]
 
-    put :update, { :id => update_id, :title => "1"}, :format => :json
+    put :update, :id => update_id, :title => "1", :format => :json
     assert_response :bad_request
 
     json_response.keys.must_include "title"
   end
 
   it "creates properly one info" do
-    post :create, { :title => "new title", :description => "new description" }, :format => :json
+    post :create, :title => "new title", :description => "new description", :format => :json
     assert_response :success
 
     NakkitypeInfo.count.must_equal 3
     assert_equal "new title", NakkitypeInfo.all.last[:title]
     assert_equal "new title", json_response["title"]
+    assert_equal "new description", NakkitypeInfo.all.last[:description]
+    assert_equal "new description", json_response["description"]
   end
 
   it "fails to create with invalid values" do
-    post :create, { :title => "1" }, :format => :json
+    post :create, :title => "1", :format => :json
     assert_response :bad_request
 
     NakkitypeInfo.count.must_equal 2
