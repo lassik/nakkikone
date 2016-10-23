856004b1e0efc6957c0fc3fc95b390314346a332
Sami Airaksinen <samiaira@gmail.com>
2016-10-23 13:27:00 +0300

    properly handle image urls in css

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,4 +1,4 @@
 Rails.application.config.requirejs.logical_asset_filter += [/\.hbs$/]
 #Rails.application.config.requirejs.logical_path_patterns += [/\.hbs$/] waits for requirejs-rails 1.x upgrade
 Rails.application.config.assets.precompile += %w( _bootstrap.min.css bootstrap-responsive.min.css datepicker.css bootstrap-wysihtml5.css bootstrap-timepicker.css print.css style.css )
-
+Rails.application.config.assets.precompile << /\.(?:png|jpg|jpeg|gif)\z/
