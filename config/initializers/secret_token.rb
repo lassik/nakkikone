ffe01618ed9975ca8340c3a0b001c15c24baa676
Sami Airaksinen <samiaira@gmail.com>
2016-10-23 12:59:05 +0300

    configure secrects via config file

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,5 +4,7 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-RailStrap::Application.config.secret_token = ENV['APP_SECRET_TOKEN']
-RailStrap::Application.config.secret_key_base = ENV['APP_SECRET_KEY_BASE']
+
+# Defined in config/secrets.yml
+# RailStrap::Application.config.secret_token = ENV['APP_SECRET_TOKEN']
+# RailStrap::Application.config.secret_key_base = ENV['APP_SECRET_KEY_BASE']
